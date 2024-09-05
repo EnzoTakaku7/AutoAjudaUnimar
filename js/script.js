6adb649ed8ead069ef3cddb3eebde3d489268a85
EnzoTakaku7 <enzo.goncalves10@etec.sp.gov.br>
2024-09-05 19:38:14 -0300

    logincadastro

--- js/script.js
+++ js/script.js
@@ -210,3 +210,301 @@ function trocarimagem18() {
 }
 
 
+
+function trocarimagem13() {
+    const imgElement = document.getElementById('imagem13');
+    const imgElement2 = document.getElementById('imagem'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Broken Heart2.png') {
+        imgElement.setAttribute('src', '../imgs/Broken Heart.png');
+        cardElement.classList.add('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Broken Heart2.png');
+        cardElement.classList.remove('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.remove('desativar'); 
+    }
+}
+
+function trocarimagem() {
+    const imgElement = document.getElementById('imagem');
+    const imgElement2 = document.getElementById('imagem13'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Favorite.png') {
+        imgElement.setAttribute('src', '../imgs/Love.png');
+        cardElement.classList.add('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Favorite.png');
+        cardElement.classList.remove('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.remove('desativar');
+    }
+}
+
+
+function trocarimagem14() {
+    const imgElement = document.getElementById('imagem14');
+    const imgElement2 = document.getElementById('imagem3'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Broken Heart2.png') {
+        imgElement.setAttribute('src', '../imgs/Broken Heart.png');
+        cardElement.classList.add('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Broken Heart2.png');
+        cardElement.classList.remove('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.remove('desativar'); 
+    }
+}
+
+function trocarimagem3() {
+    const imgElement = document.getElementById('imagem3');
+    const imgElement2 = document.getElementById('imagem14'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Favorite.png') {
+        imgElement.setAttribute('src', '../imgs/Love.png');
+        cardElement.classList.add('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Favorite.png');
+        cardElement.classList.remove('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.remove('desativar');
+    }
+}
+
+
+function trocarimagem15() {
+    const imgElement = document.getElementById('imagem15');
+    const imgElement2 = document.getElementById('imagem5'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Broken Heart2.png') {
+        imgElement.setAttribute('src', '../imgs/Broken Heart.png');
+        cardElement.classList.add('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Broken Heart2.png');
+        cardElement.classList.remove('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.remove('desativar'); 
+    }
+}
+
+function trocarimagem5() {
+    const imgElement = document.getElementById('imagem5');
+    const imgElement2 = document.getElementById('imagem15'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Favorite.png') {
+        imgElement.setAttribute('src', '../imgs/Love.png');
+        cardElement.classList.add('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Favorite.png');
+        cardElement.classList.remove('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.remove('desativar');
+    }
+}
+
+
+
+
+function trocarimagem16() {
+    const imgElement = document.getElementById('imagem16');
+    const imgElement2 = document.getElementById('imagem7'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Broken Heart2.png') {
+        imgElement.setAttribute('src', '../imgs/Broken Heart.png');
+        cardElement.classList.add('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Broken Heart2.png');
+        cardElement.classList.remove('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.remove('desativar'); 
+    }
+}
+
+function trocarimagem7() {
+    const imgElement = document.getElementById('imagem7');
+    const imgElement2 = document.getElementById('imagem16'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Favorite.png') {
+        imgElement.setAttribute('src', '../imgs/Love.png');
+        cardElement.classList.add('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Favorite.png');
+        cardElement.classList.remove('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.remove('desativar');
+    }
+}
+
+
+
+
+
+function trocarimagem17() {
+    const imgElement = document.getElementById('imagem17');
+    const imgElement2 = document.getElementById('imagem9'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Broken Heart2.png') {
+        imgElement.setAttribute('src', '../imgs/Broken Heart.png');
+        cardElement.classList.add('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Broken Heart2.png');
+        cardElement.classList.remove('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.remove('desativar'); 
+    }
+}
+
+function trocarimagem9() {
+    const imgElement = document.getElementById('imagem9');
+    const imgElement2 = document.getElementById('imagem17'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Favorite.png') {
+        imgElement.setAttribute('src', '../imgs/Love.png');
+        cardElement.classList.add('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Favorite.png');
+        cardElement.classList.remove('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.remove('desativar');
+    }
+}
+
+
+
+
+
+
+function trocarimagem18() {
+    const imgElement = document.getElementById('imagem18');
+    const imgElement2 = document.getElementById('imagem11'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Broken Heart2.png') {
+        imgElement.setAttribute('src', '../imgs/Broken Heart.png');
+        cardElement.classList.add('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Broken Heart2.png');
+        cardElement.classList.remove('vermelho-outline');
+        cardElement.classList.remove('verdin-outline');
+        imgElement2.classList.remove('desativar'); 
+    }
+}
+
+function trocarimagem11() {
+    const imgElement = document.getElementById('imagem11');
+    const imgElement2 = document.getElementById('imagem18'); 
+    const cardElement = imgElement.closest('.card');
+    const currentSrc = imgElement.getAttribute('src');
+
+    if (currentSrc === '../imgs/Favorite.png') {
+        imgElement.setAttribute('src', '../imgs/Love.png');
+        cardElement.classList.add('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.add('desativar'); 
+    } else {
+        imgElement.setAttribute('src', '../imgs/Favorite.png');
+        cardElement.classList.remove('verdin-outline');
+        cardElement.classList.remove('vermelho-outline');
+        imgElement2.classList.remove('desativar');
+    }
+}
+
+
+
+
+
+
+
+
+function cadastrarUsuario() {
+    const username = document.getElementById('cadastro-nome').value;
+    const email = document.getElementById('cadastro-email').value;
+    const password = document.getElementById('cadastro-senha').value;
+    const confirmPassword = document.getElementById('cadastro-confirmarsenha').value;
+
+    if (password !== confirmPassword) {
+        alert("As senhas não coincidem");
+        return;
+    }
+
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    const usuarioExistente = usuarios.find((user) => user.username === username);
+    if (usuarioExistente) {
+        alert("Usuário já existe");
+        return;
+    }
+
+    const novoUsuario = {
+        username: username,
+        email: email,
+        password: password
+    };
+    usuarios.push(novoUsuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    alert("Usuário cadastrado");
+    window.location.href = "login.html";
+}
+
+function login() {
+    const username = document.getElementById('login-nome').value;
+    const password = document.getElementById('login-senha').value;
+
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    const usuarioEncontrado = usuarios.find(
+        (user) => user.username === username && user.password === password
+    );
+
+    if (usuarioEncontrado) {
+        alert("Login aprovado");
+        window.location.href = "home.html"; 
+    } else {
+        alert("Usuário ou senha incorretos");
+    }
+}
